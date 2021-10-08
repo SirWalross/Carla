176ad9e7498b1dca43008b1794b4a613f8b9a7bd
SirWalross <27106448+SirWalross@users.noreply.github.com>
2021-10-08 11:06:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,51 @@
 import carla
 import random
+import argparse
+
 
 def do_something(data):
     print(data)
 
-try:
-    client = carla.Client('172.23.48.1', 2000)
-    client.set_timeout(10.0)
-    world = client.load_world('Town02')
-    blueprint_library = world.get_blueprint_library()
-    spawn_points = world.get_map().get_spawn_points()
-    spawn_point = random.choice(spawn_points)
-    spawn_point.z = 10
-    spawn_point.location = world.get_random_location_from_navigation()
-    vehicle_bp = blueprint_library.find('vehicle.mercedes.sprinter')
-    print(vehicle_bp)
-    print(spawn_point)
-    vehicle = world.spawn_actor(vehicle_bp, spawn_point)
-    camera_bp = blueprint_library.find('sensor.camera.rgb')
 
-    # Modify the attributes of the blueprint to set image resolution and field of view.
-    camera_bp.set_attribute('image_size_x', '1280')
-    camera_bp.set_attribute('image_size_y', '720')
-    camera_bp.set_attribute('fov', '110')
-    # Set the time in seconds between sensor captures
-    camera_bp.set_attribute('sensor_tick', '10.0')
+def main(ip: str):
+    try:
+        client = carla.Client(ip, 2000)
+        client.set_timeout(10.0)
+        world = client.load_world("Town02")
+        map = world.get_map()
+        blueprint_library = world.get_blueprint_library()
+        spawn_points = world.get_map().get_spawn_points()
+        spawn_point = random.choice(spawn_points)
+        spawn_point.z = 10
+        spawn_point.location = world.get_random_location_from_navigation()
+        vehicle_bp = blueprint_library.find("vehicle.mercedes.sprinter")
+        print(vehicle_bp)
+        print(spawn_point)
+        vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+        camera_bp = blueprint_library.find("sensor.camera.rgb")
 
-    relative_transform = carla.Transform(carla.Location(x=0, y=0, z=0), carla.Rotation(yaw=0))
-    camera = world.spawn_actor(camera_bp, relative_transform, vehicle)
-    camera.listen(lambda data: do_something(data))
-    while 1:
-        pass
-except KeyboardInterrupt as e:
-    vehicle.destroy()
-    print("\nFinished")
-    raise e
+        # Modify the attributes of the blueprint to set image resolution and field of view.
+        camera_bp.set_attribute("image_size_x", "1280")
+        camera_bp.set_attribute("image_size_y", "720")
+        camera_bp.set_attribute("fov", "110")
+        # Set the time in seconds between sensor captures
+        camera_bp.set_attribute("sensor_tick", "10.0")
+
+        relative_transform = carla.Transform(carla.Location(x=0, y=0, z=0), carla.Rotation(yaw=0))
+        camera = world.spawn_actor(camera_bp, relative_transform, vehicle)
+        camera.listen(lambda data: do_something(data))
+
+        waypoint_list = map.generate_waypoints(2.0)
+        while 1:
+            pass
+    except KeyboardInterrupt as e:
+        vehicle.destroy()
+        print("\nFinished")
+        raise e
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host", nargs="?", default="127.0.0.1", help="IP of the host server (default: 127.0.0.1)")
+    args = parser.parse_args()
+    main(args.host)
