15d70258e21629a200431093adae54e61bd3281f
SirWalross <27106448+SirWalross@users.noreply.github.com>
2021-11-26 21:49:51 +0100

    Improve search for paths

--- parse_xodr.py
+++ parse_xodr.py
@@ -1,11 +1,157 @@
+from time import time
 from typing import Dict, List, Optional, Tuple
 import untangle
 import matplotlib.pyplot as plt
 import numpy as np
 
 
+class Road:
+    def __init__(self, length: float, id: int, junction: int, name: str) -> None:
+        self.length = length
+        self.id = id
+        self.junction = junction
+        self.name = name
+        self.predecessor: Optional[RoadLink] = None
+        self.successor: Optional[RoadLink] = None
+        self.ref_line = RefLine(self.length)
+        self.lane_sections: Dict[float, LaneSection] = {}  # from s0 to lane section
+        self.offset: Dict[float, Poly3] = {}  # from s to poly3
+    
+    def next_lane(self, lane: "Lane") -> Optional["Lane"]:
+        assert(lane.lane_section.road == self)
+        travelDir = lane.direction
+        lanes = [lane_section.lanes[lane.id] for lane_section in list(self.lane_sections.values())]
+
+        index = lanes.index(lane)
+        if travelDir == "forward":
+            if len(lanes) - 1 != index:
+                return lanes[index + 1]
+            else:
+                return None
+        else:
+            if index != 0:
+                return lanes[index - 1]
+            else:
+                return None
+
+class LaneLink():
+    def __init__(self, from_id: int, to_id: int):
+        self.from_id = from_id
+        self.to_id = to_id
+
+class Connection():
+    def __init__(self, id: int, incomingRoad: int, connectingRoad: int, contactPoint: str):
+        self.id = id
+        self.incomingRoad = incomingRoad
+        self.connectionRoad = connectingRoad
+        self.contactPoint = contactPoint
+        self.lane_links: Dict[int, LaneLink] = {}
+
+class Junction:
+    def __init__(self, id: int):
+        self.id = id
+        self.connections: Dict[int, Connection] = {}
+
+class RefLine:
+    def __init__(self, length: float) -> None:
+        self.length = length
+        self.geometries: Dict[float, RoadGeometry] = {}  # from s0 to road geometry
+
+
+class RoadLink:
+    def __init__(self, elementId: int, elementType: str, contactPoint: str) -> None:
+        self.id = elementId
+        self.type = elementType
+        self.contactPoint = contactPoint
+
+
+class RoadGeometry:
+    def __init__(self, s: float, x: float, y: float, hdg: float, length: float) -> None:
+        self.s = s
+        self.x = x
+        self.y = y
+        self.hdg = hdg
+        self.length = length
+
+    def get_xy(self, s: float) -> Tuple[float, float]:
+        raise NotImplementedError()
+
+    def get_grad(self, s: float) -> Tuple[float, float]:
+        raise NotImplementedError()
+
+
+class Arc(RoadGeometry):
+    def __init__(self, s: float, x: float, y: float, hdg: float, length: float, curvature: float) -> None:
+        super().__init__(s, x, y, hdg, length)
+        self.curvature = curvature
+
+    def get_xy(self, s: float) -> Tuple[float, float]:
+        angle_at_s = (s - self.s) * self.curvature - np.pi / 2
+        r = 1 / self.curvature
+        xs = r * (np.cos(self.hdg + angle_at_s) - np.sin(self.hdg)) + self.x
+        ys = r * (np.sin(self.hdg + angle_at_s) + np.cos(self.hdg)) + self.y
+        return (xs, ys)
+
+    def get_grad(self, s: float) -> Tuple[float, float]:
+        return (
+            np.sin((np.pi / 2) - self.curvature * (s - self.s) - self.hdg),
+            np.cos((np.pi / 2) - self.curvature * (s - self.s) - self.hdg),
+        )
+
+
+class Line(RoadGeometry):
+    def __init__(self, s: float, x: float, y: float, hdg: float, length: float) -> None:
+        super().__init__(s, x, y, hdg, length)
+
+    def get_xy(self, s: float) -> Tuple[float, float]:
+        return (np.cos(self.hdg) * (s - self.s) + self.x, np.sin(self.hdg) * (s - self.s) + self.y)
+
+    def get_grad(self, s: float) -> Tuple[float, float]:
+        return (np.cos(self.hdg), np.sin(self.hdg))
+
+
+class LaneSection:
+    def __init__(self, s: float, length: float) -> None:
+        self.s = s
+        self.road: Road = None
+        self.lanes: Dict[int, Lane] = {}  # from id to lane
+        self.length = length
+
+
+class Lane:
+    def __init__(self, id: int, level: bool, type: str, direction: str) -> None:
+        self.id = id
+        self.level = level
+        self.type = type
+        self.direction = direction
+        self.road: Road = None
+        self.widths: Dict[float, Poly3] = {}  # from s to poly3
+        self.predecessor: int = -1
+        self.successor: int = -1
+        self.lane_section: LaneSection = None
+
+
+class Poly3:
+    def __init__(self, s: float, a: float, b: float, c: float, d: float) -> None:
+        self.s = s
+        self.a = a - b * s + c * s * s - d * s * s * s
+        self.b = b - 2 * c * s + 3 * d * s * s
+        self.c = c - 3 * d * s
+        self.d = d
+
+    def get(self, s: float):
+        return self.a + self.b * s + self.c * s * s + self.d * s * s * s
+
+    def get_grad(self, s: float):
+        return self.b + 2 * self.c * s + 3 * self.d * s * s
+
+    def negate(self) -> "Poly3":
+        return Poly3(self.s, -self.a, -self.b, -self.c, -self.d)
+
+
 class OpenDriveMap:
     def __init__(self, xodr_file: str):
+        self.possible_paths: List[List[Lane]] = []
 
         self.xodr = untangle.parse(xodr_file)
 
@@ -15,14 +161,11 @@ class OpenDriveMap:
 
             # parse road links
             for link in ["predecessor", "successor"]:
-                try:
-                    road_link_node = getattr(road_node.link, link)
-                    road_link = RoadLink(
-                        road_link_node["elementId"], road_link_node["elementType"], road_link_node["contactPoint"]
-                    )
-                    setattr(road, link, road_link)
-                except AttributeError:
-                    pass
+                road_link_node = getattr(road_node.link, link)
+                road_link = RoadLink(
+                    int(road_link_node["elementId"]), road_link_node["elementType"], road_link_node["contactPoint"]
+                )
+                setattr(road, link, road_link)
 
             # parse road geometries
             for geometry_hdr_node in road_node.planView.geometry:
@@ -84,9 +227,11 @@ class OpenDriveMap:
 
                     if lane_type != "driving":
                         continue
-                    lane = Lane(lane_id, level, lane_type)
+                    direction = lane_node.userData.vectorLane[0]["travelDir"] if isinstance(lane_node.userData.vectorLane, list) else lane_node.userData.vectorLane["travelDir"]
+                    lane = Lane(lane_id, level, lane_type, direction)
                     lane.road = road
                     lane_section.lanes[lane_id] = lane
+                    lane.lane_section = lane_section
 
                     try:
                         for lane_width_node in lane_node.width:
@@ -110,6 +255,162 @@ class OpenDriveMap:
             if all([not bool(lane_section.lanes) for _, lane_section in road.lane_sections.items()]):
                 continue
             self.roads.append(road)
+
+        self.junctions: List[Junction] = []
+        for junction_node in self.xodr.OpenDRIVE.junction:
+            junction = Junction(int(junction_node["id"]))
+
+            # parse connections
+            for connection_node in junction_node.connection:
+                id = int(connection_node["id"])
+                connection = Connection(id, int(connection_node["incomingRoad"]), int(connection_node["connectingRoad"]), connection_node["contactPoint"])
+
+                # parse lane links
+                for lane_link_node in connection_node.laneLink:
+                    lane_link = LaneLink(int(lane_link_node["from"]), int(lane_link_node["to"]))
+
+                    connection.lane_links[lane_link.from_id] = lane_link
+                
+                junction.connections[id] = connection
+            
+            self.junctions.append(junction)
+    
+    def _highlight_lanes(self, lanes: List[Lane], color = 'orange', eps: float = 0.1):
+        roads = [lane.lane_section.road for lane in lanes]
+        # highlight lanes
+        for road in roads:
+            ref_line_xy = [[], [], [], [], []]  # x, y, dx, dy
+            for _, geometry in road.ref_line.geometries.items():
+                s = np.arange(geometry.s, geometry.s + geometry.length, eps)
+                get_xy = np.vectorize(geometry.get_xy)
+                get_grad = np.vectorize(geometry.get_grad)
+                xy = get_xy(s)
+                grad = get_grad(s)
+                ref_line_xy[0].extend(s)
+                ref_line_xy[1].extend(xy[0])
+                ref_line_xy[2].extend(xy[1])
+                ref_line_xy[3].extend(grad[0])
+                ref_line_xy[4].extend(grad[1])
+
+                if isinstance(geometry, Arc):
+                    pass
+            ref_line_xy = np.array(ref_line_xy)
+            for _, lane_section in road.lane_sections.items():
+                s = np.arange(lane_section.s, lane_section.s + lane_section.length, eps)
+                keys = list(road.offset.keys())
+                offset = [road.offset[keys[np.searchsorted(keys, s0, side="right") - 1]].get(s0) for s0 in s]
+                ref_line_section_xy = np.array(
+                    [ref_line_xy[1:, np.searchsorted(ref_line_xy[0], s0, side="right") - 1] for s0 in s]
+                )
+                for _, lane in lane_section.lanes.items():
+                    if lane not in lanes:
+                        continue
+                    s = np.arange(0, lane_section.length, eps)
+                    keys = list(lane.widths.keys())
+                    if lane.id > 0:
+                        width = [
+                            offset[i] + lane.widths[keys[np.searchsorted(keys, s0, side="right") - 1]].get(s0)
+                            for i, s0 in enumerate(s)
+                        ]
+                    else:
+                        width = [
+                            offset[i] + lane.widths[keys[np.searchsorted(keys, s0, side="right") - 1]].negate().get(s0)
+                            for i, s0 in enumerate(s)
+                        ]
+                    xy = np.array(
+                        [
+                            (
+                                ref_line_section_xy[i, 0] - ref_line_section_xy[i, 3] * width[i]/2,
+                                ref_line_section_xy[i, 1] + ref_line_section_xy[i, 2] * width[i]/2,
+                            )
+                            for i, _ in enumerate(s)
+                        ]
+                    )
+                    plt.plot(xy[:, 0], xy[:, 1], c=color)
+
+
+    def find_route(self, road1: str, section1: int, lane1: int, road2: str, section2: int, lane2: int):
+        road1: Road = next(road for road in self.roads if road.name == road1)
+        section1: LaneSection = list(road1.lane_sections.values())[section1]
+        lane1: Lane = next(lane for lane in section1.lanes.values() if lane.id == lane1)
+        road2: Road = next(road for road in self.roads if road.name == road2)
+        section2: LaneSection = list(road2.lane_sections.values())[section2]
+        lane2: Lane = next(lane for lane in section2.lanes.values() if lane.id == lane2)
+
+        curr = time()
+        try:
+            self._dfs([], lane1, [lane1], lane2)
+        except ValueError:
+            if len(self.possible_paths) == 0:
+                raise ValueError("Didnt find possible path")
+        print(f"Finding of route, with {len(self.possible_paths)} possible routes, took {time() - curr}s")
+        shortest_path = sorted(self.possible_paths, key=lambda path: sum([lane.lane_section.length for lane in path]))[0]
+
+        self._highlight_lanes(shortest_path)
+        self._highlight_lanes([lane1, lane2], color='black')
+
+    def _dfs(self, visited_lanes: List[Lane], current_lane: Lane, path: List[Lane], goal: Lane):
+        current_road = current_lane.lane_section.road
+        current_section = current_lane.lane_section
+
+        next_lanes = []
+        next_lane = current_road.next_lane(current_lane)
+        if next_lane is None:
+            # already at last lane_section
+            link = "successor" if current_lane.direction == "forward" else "predecessor"
+            next_road_link = getattr(current_road, link)
+            if next_road_link.type == "road":
+                # next road is of type road
+                next_road = self._find_road(next_road_link.id)
+
+                if (next_road_link.contactPoint != "start") != (link != "successor"):
+                    # flip next road 
+                    next_lane = list(next_road.lane_sections.values())[-1].lanes[-current_lane.id]
+                    next_lanes.append(next_lane)
+                else:
+                    next_lanes.append(next_road.lane_sections[0].lanes[current_lane.id])
+            else:
+                # next road is of type junction
+                junction = self._find_junction(next_road_link.id)
+                
+                # find next roads
+                next_roads = []
+                for connection in junction.connections.values():
+                    if connection.incomingRoad == current_road.id:
+                        try:
+                            next_roads.append((self._find_road(connection.connectionRoad), connection.lane_links))
+                        except StopIteration:
+                            continue
+
+                for next_road, lane_links in next_roads:
+                    # find next lane
+                    next_lanes.append(next_road.lane_sections[0].lanes[lane_links[current_lane.id].to_id])
+        else:
+            next_lanes.append(next_lane)
+        
+        for next_lane in next_lanes:
+            if goal == next_lane:
+                self.possible_paths.append([*path, next_lane])
+
+                # find all possible paths
+                # if len(self.possible_paths) > 4:
+                #     path.append(next_lane)
+                #     return path
+            if next_lane in path:
+                continue
+            else:
+                try:
+                    new_path = self._dfs([*visited_lanes, next_lane], next_lane, [*path, next_lane], goal)
+                    return new_path
+                except ValueError:
+                    continue
+        raise ValueError("No more lanes to explore")
+
+    def _find_road(self, roadId: int) -> Road:
+        return next(road for road in self.roads if road.id == roadId)
+    
+    def _find_junction(self, junctionId: int) -> Junction:
+        return next(junction for junction in self.junctions if junction.id == junctionId)
 
     def render(self, eps: float = 0.1):
         # plt.ion()
@@ -162,117 +463,12 @@ class OpenDriveMap:
                         ]
                     )
                     plt.plot(xy[:, 0], xy[:, 1], "g")
-        plt.show()
-
-
-class Road:
-    def __init__(self, length: float, id: int, junction: int, name: str) -> None:
-        self.length = length
-        self.id = id
-        self.junction = junction
-        self.name = name
-        self.predecessor: Optional[RoadLink] = None
-        self.successor: Optional[RoadLink] = None
-        self.ref_line = RefLine(self.length)
-        self.lane_sections: Dict[float, LaneSection] = {}  # from s0 to lane section
-        self.offset: Dict[float, Poly3] = {}  # from s to poly3
-
-
-class RefLine:
-    def __init__(self, length: float) -> None:
-        self.length = length
-        self.geometries: Dict[float, RoadGeometry] = {}  # from s0 to road geometry
-
-
-class RoadLink:
-    def __init__(self, elementId: str, elementType: str, contactPoint: str) -> None:
-        self.id = elementId
-        self.side = elementType
-        self.direction = contactPoint
-
-
-class RoadGeometry:
-    def __init__(self, s: float, x: float, y: float, hdg: float, length: float) -> None:
-        self.s = s
-        self.x = x
-        self.y = y
-        self.hdg = hdg
-        self.length = length
-
-    def get_xy(self, s: float) -> Tuple[float, float]:
-        raise NotImplementedError()
-
-    def get_grad(self, s: float) -> Tuple[float, float]:
-        raise NotImplementedError()
-
-
-class Arc(RoadGeometry):
-    def __init__(self, s: float, x: float, y: float, hdg: float, length: float, curvature: float) -> None:
-        super().__init__(s, x, y, hdg, length)
-        self.curvature = curvature
-
-    def get_xy(self, s: float) -> Tuple[float, float]:
-        angle_at_s = (s - self.s) * self.curvature - np.pi / 2
-        r = 1 / self.curvature
-        xs = r * (np.cos(self.hdg + angle_at_s) - np.sin(self.hdg)) + self.x
-        ys = r * (np.sin(self.hdg + angle_at_s) + np.cos(self.hdg)) + self.y
-        return (xs, ys)
-
-    def get_grad(self, s: float) -> Tuple[float, float]:
-        return (
-            np.sin((np.pi / 2) - self.curvature * (s - self.s) - self.hdg),
-            np.cos((np.pi / 2) - self.curvature * (s - self.s) - self.hdg),
-        )
-
-
-class Line(RoadGeometry):
-    def __init__(self, s: float, x: float, y: float, hdg: float, length: float) -> None:
-        super().__init__(s, x, y, hdg, length)
-
-    def get_xy(self, s: float) -> Tuple[float, float]:
-        return (np.cos(self.hdg) * (s - self.s) + self.x, np.sin(self.hdg) * (s - self.s) + self.y)
-
-    def get_grad(self, s: float) -> Tuple[float, float]:
-        return (np.cos(self.hdg), np.sin(self.hdg))
-
-
-class LaneSection:
-    def __init__(self, s: float, length: float) -> None:
-        self.s = s
-        self.road: Road = None
-        self.lanes: Dict[int, Lane] = {}  # from id to lane
-        self.length = length
-
-
-class Lane:
-    def __init__(self, id: int, level: bool, type: str) -> None:
-        self.id = id
-        self.level = level
-        self.type = type
-        self.road: Road = None
-        self.widths: Dict[float, Poly3] = {}  # from s to poly3
-        self.predecessor: int = -1
-        self.successor: int = -1
-
-
-class Poly3:
-    def __init__(self, s: float, a: float, b: float, c: float, d: float) -> None:
-        self.s = s
-        self.a = a - b * s + c * s * s - d * s * s * s
-        self.b = b - 2 * c * s + 3 * d * s * s
-        self.c = c - 3 * d * s
-        self.d = d
-
-    def get(self, s: float):
-        return self.a + self.b * s + self.c * s * s + self.d * s * s * s
-
-    def get_grad(self, s: float):
-        return self.b + 2 * self.c * s + 3 * self.d * s * s
-
-    def negate(self) -> "Poly3":
-        return Poly3(self.s, -self.a, -self.b, -self.c, -self.d)
 
 
 if __name__ == "__main__":
-    open_drive_map = OpenDriveMap("OpenDriveMaps/map07.xodr")
+    plt.ion()
+    plt.show()
+    open_drive_map = OpenDriveMap("OpenDriveMaps/map02.xodr")
     open_drive_map.render()
+    open_drive_map.find_route("Road 19", 0, -1, "Road 1", 0, -1)
+    plt.show()
