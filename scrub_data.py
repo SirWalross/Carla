2cbb25aa9a99b1832d0f7e00f864fc375784129d
SirWalross <27106448+SirWalross@users.noreply.github.com>
2022-01-03 11:29:32 +0100

    Update scrub_data.py

--- scrub_data.py
+++ scrub_data.py
@@ -7,68 +7,34 @@ import carla
 import cv2
 import os
 import shortuuid
+import time
 
 from carla import ColorConverter as cc
 
 random.seed(42)
 
-written_images = 0
 MAX_IMAGES = 10000
 WIDTH = 1280
 HEIGHT = 720
 BORDER = 0.1
-traffic_light = None
-traffic_light = []
+TRAFFIC_SIGN_DETECTION_RANGE = (500, 1200)  # min and max area of sign
 traffic_sign = None
-traffic_signs = []
 vehicle = None
 
+signs = []
+current_speed = None
 
-def get_yaw_diff(traffic_sign_vector, vehicle_vector) -> int:
-    traffic_sign_vector = np.array([traffic_sign_vector.x - vehicle_vector.x, traffic_sign_vector.y - vehicle_vector.y, 0.0])
-    vehicle_vector = np.array([vehicle_vector.x, vehicle_vector.y, 0.0])
-    wv_linalg = np.linalg.norm(traffic_sign_vector) * np.linalg.norm(vehicle_vector)
-    if wv_linalg == 0:
-        _dot = 1
-    else:
-        _dot = math.acos(np.clip(np.dot(traffic_sign_vector, vehicle_vector) / (wv_linalg), -1.0, 1.0))
-
-    _cross = np.cross(vehicle_vector, traffic_sign_vector)
-    if _cross[2] < 0:
-        _dot *= -1.0
-    return int(_dot * 180 / np.pi)
-
-
-def get_traffic_sign_type() -> Tuple[int, int]:
-    # offset vehicle location by 10 m in forward direction and 1 m to the right
-    location = vehicle.get_location() + vehicle.get_transform().get_forward_vector() * 10 + vehicle.get_transform().get_right_vector()
-
-    distances = [sign.get_location().distance(location) for sign in traffic_signs]
-    index = np.argmin(distances)
-    yaw_diff = get_yaw_diff(traffic_signs[index].get_transform().get_forward_vector(), vehicle.get_transform().get_forward_vector())
-
-    if abs(yaw_diff) < 180:
-        return int(traffic_signs[index].type_id[20:]), yaw_diff
-    else:
-        return -1, yaw_diff
-
-
-def get_traffic_light_type() -> Tuple[int, int]:
-    # offset vehicle location by 10 m in forward direction and 1 m to the right
-    location = vehicle.get_location() + vehicle.get_transform().get_forward_vector() * 10 + vehicle.get_transform().get_right_vector()
-
-    distances = [light.get_location().distance(location) for light in traffic_lights]
-    index = np.argmin(distances)
-    yaw_diff = get_yaw_diff(traffic_lights[index].get_transform().get_forward_vector(), vehicle.get_transform().get_forward_vector())
-
-    if abs(yaw_diff) < 180:
-        return traffic_lights[index].state, yaw_diff
-    else:
-        return carla.TrafficLightState.Off, yaw_diff
+def write_signs_to_disk(sign_type: float):
+    sign_type = int(3.6 * sign_type)
+    global signs
+    for sign in signs:
+        os.makedirs(f"sign/{sign_type}", exist_ok=True)
+        cv2.imwrite(f"sign/{sign_type}/{shortuuid.uuid()}.png", sign)
+    print(f"Written {len(signs)} speed {sign_type} signs")
+    signs = []
 
 
 def rgb_sensor(image):
-    global written_images
     image.convert(cc.Raw)
     array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
     array = np.reshape(array, (image.height, image.width, 4))
@@ -77,99 +43,74 @@ def rgb_sensor(image):
     array = np.array(array)
     raw_image = np.copy(array)
 
-    if traffic_light is not None and len(traffic_light) > 0:
-        x, y, w, h = cv2.boundingRect(traffic_light)
-        x1 = int(np.clip(x - w * BORDER, 0, WIDTH))
-        x2 = int(np.clip(x + w * (1 + BORDER), 0, WIDTH))
-        y1 = int(np.clip(y - h * BORDER, 0, HEIGHT))
-        y2 = int(np.clip(y + h * (1 + BORDER), 0, HEIGHT))
-        image = raw_image[y1:y2, x1:x2, :]
-
-        light_type, angle = get_traffic_light_type()
-        if x + w <= WIDTH - 2 * w * BORDER and w > 16:
-            os.makedirs(f"traffic_light/{light_type}", exist_ok=True)
-            cv2.imwrite(f"traffic_light/{light_type}/{written_images}.{angle:.2f}.{x + w}.png", image)
-            written_images += 1
-
     if traffic_sign is not None:
-        x, y, w, h = cv2.boundingRect(traffic_sign)
-        x1 = int(np.clip(x - w * BORDER, 0, WIDTH))
-        x2 = int(np.clip(x + w * (1 + BORDER), 0, WIDTH))
-        y1 = int(np.clip(y - h * BORDER, 0, HEIGHT))
-        y2 = int(np.clip(y + h * (1 + BORDER), 0, HEIGHT))
-        image = raw_image[y1:y2, x1:x2, :]
+        area = cv2.contourArea(traffic_sign)
 
-        sign_type, angle = get_traffic_sign_type()
-        if x + w <= WIDTH - 2 * w * BORDER and w > 16:
-            os.makedirs(f"traffic_sign/{sign_type}", exist_ok=True)
-            cv2.imwrite(f"traffic_sign/{sign_type}/{written_images}.{angle:.2f}.{x + w}.png", image)
-            written_images += 1
+        if area >= TRAFFIC_SIGN_DETECTION_RANGE[0] and area <= TRAFFIC_SIGN_DETECTION_RANGE[1]:
+            x, y, w, h = cv2.boundingRect(traffic_sign)
+            x1 = int(np.clip(x - w * BORDER, 0, WIDTH))
+            x2 = int(np.clip(x + w * (1 + BORDER), 0, WIDTH))
+            y1 = int(np.clip(y - h * BORDER, 0, HEIGHT))
+            y2 = int(np.clip(y + h * (1 + BORDER), 0, HEIGHT))
+            image = raw_image[y1:y2, x1:x2, :]
+
+            if x + w <= WIDTH - 2 * w * BORDER and w > 16:
+                signs.append(image)
 
 
 def segmentation_sensor(image):
-    global traffic_light, traffic_sign
+    global traffic_sign
     image.convert(cc.CityScapesPalette)
     array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
     array = np.reshape(array, (image.height, image.width, 4))
     array = array[:, :, :3]
-    array = np.array(array[:, :, ::-1], dtype=np.dtype("uint8"))
-
-    # Traffic light
-    traffic_image = np.copy(array)
-    # preparing the mask to overlay
-    mask = cv2.inRange(traffic_image, np.array([249, 169, 29]), np.array([251, 171, 31]))
-
-    base_colour = np.full_like(traffic_image, np.array([255, 255, 255]))
-    traffic_image = cv2.bitwise_and(base_colour, base_colour, mask=mask)
-
-    # convert image to greyscale
-    traffic_image = cv2.cvtColor(traffic_image, cv2.COLOR_BGR2GRAY)
-
-    # contour detection
-    contours, _ = cv2.findContours(traffic_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    if len(contours) > 0:
-        traffic_light = max(contours, key=cv2.contourArea)
-        print("detected light")
-    else:
-        traffic_light = None
+    array = array[:, :, ::-1]
 
     # Traffic sign
-    traffic_image = np.copy(array)
-    # preparing the mask to overlay
-    mask = cv2.inRange(traffic_image, np.array([219, 219, 0]), np.array([221, 221, 1]))
 
-    base_colour = np.full_like(traffic_image, np.array([255, 255, 255]))
-    traffic_image = cv2.bitwise_and(base_colour, base_colour, mask=mask)
+    # preparing the mask to overlay
+    mask = cv2.inRange(array, np.array([219, 219, 0]), np.array([221, 221, 1]))
+
+    base_colour = np.full_like(array, np.array([255, 255, 255]))
+    array = cv2.bitwise_and(base_colour, base_colour, mask=mask)
 
     # create a zero array
-    stencil = np.zeros_like(traffic_image[:, :, 0])
+    stencil = np.zeros_like(array[:, :, 0])
 
     # specify coordinates of the polygon
     polygon = np.array([[int(WIDTH / 2), 200], [int(WIDTH / 2), HEIGHT], [WIDTH, HEIGHT], [WIDTH, 200]])
     cv2.fillConvexPoly(stencil, polygon, 255)
 
     # convert image to greyscale
-    traffic_image = cv2.bitwise_and(traffic_image, traffic_image, mask=stencil)
-    traffic_image = cv2.cvtColor(traffic_image, cv2.COLOR_BGR2GRAY)
+    array = cv2.bitwise_and(array, array, mask=stencil)
+    array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
 
     # contour detection
-    contours, _ = cv2.findContours(traffic_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(array, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     if len(contours) > 0:
         traffic_sign = max(contours, key=cv2.contourArea)
-        print(f"detected sign")
     else:
         traffic_sign = None
 
 
 def main(ip: str):
-    global vehicle, traffic_lights, traffic_signs
+    global vehicle, current_speed
+    now = time.time()
+    segmentation = None
+    rgb = None
+    vehicle = None
     try:
         client = carla.Client(ip, 2000)
         client.set_timeout(10.0)
         world = client.load_world("Town02")
         # world = client.get_world()
+        
+        # Settings
+        settings = world.get_settings()
+        settings.synchronous_mode = True  # Enables synchronous mode
+        settings.fixed_delta_seconds = 0.05
+        world.apply_settings(settings)
 
         map = world.get_map()
         blueprint_library = world.get_blueprint_library()
@@ -184,6 +125,7 @@ def main(ip: str):
             except RuntimeError:
                 pass
         print("Spawned vehicle")
+        current_speed = vehicle.get_speed_limit()
 
         # RGB camera
         rgb_bp = blueprint_library.find("sensor.camera.rgb")
@@ -204,20 +146,23 @@ def main(ip: str):
         segmentation = world.spawn_actor(segmentation_bp, relative_transform, vehicle)
         segmentation.listen(segmentation_sensor)
 
-        traffic_signs = [world.get_traffic_sign(landmark) for landmark in map.get_all_landmarks_of_type("274")]
-        traffic_lights = [world.get_traffic_light(landmark) for landmark in map.get_all_landmarks_of_type("1000001")]
-
         vehicle.set_autopilot()
 
-        while written_images < MAX_IMAGES:
-            pass
+        while time.time() - now > 60.0 * 1:
+            speed = vehicle.get_speed_limit()
+            if current_speed != speed:
+                current_speed = speed
+                write_signs_to_disk(current_speed)
+            world.tick()
+        raise ValueError()
     finally:
-        try:
+        if segmentation is not None:
             segmentation.destroy()
+        if rgb is not None:
             rgb.destroy()
+        if vehicle is not None:
             vehicle.destroy()
-        except UnboundLocalError:
-            pass
+        print("Cleaned up")
 
 
 if __name__ == "__main__":
